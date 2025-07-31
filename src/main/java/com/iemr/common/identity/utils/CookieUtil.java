ca3a4659df4ec09adf304bd7cba4eb5436ebb2d3
Ravi Shanigarapu <ravi.shanigarapu@wipro.com>
2025-07-31 11:17:22 +0530

    removed unused method

--- src/main/java/com/iemr/common/identity/utils/CookieUtil.java
+++ src/main/java/com/iemr/common/identity/utils/CookieUtil.java
@@ -1,14 +1,11 @@
 package com.iemr.common.identity.utils;
 
-import java.util.Arrays;
 import java.util.Optional;
 
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 
 import jakarta.servlet.http.Cookie;
 import jakarta.servlet.http.HttpServletRequest;
-import jakarta.servlet.http.HttpServletResponse;
 
 @Service
 public class CookieUtil {
@@ -23,17 +20,6 @@ public class CookieUtil {
 			}
 		}
 		return Optional.empty();
-	}
-
-	public void addJwtTokenToCookie(String Jwttoken, HttpServletResponse response, HttpServletRequest request) {
-		Cookie cookie = new Cookie("Jwttoken", Jwttoken);
-		cookie.setHttpOnly(true);
-		cookie.setMaxAge(60 * 60 * 24);
-		cookie.setPath("/");
-		if ("https".equalsIgnoreCase(request.getScheme())) {
-			cookie.setSecure(true);
-		}
-		response.addCookie(cookie);
 	}
 
 	public String getJwtTokenFromCookie(HttpServletRequest request) {
